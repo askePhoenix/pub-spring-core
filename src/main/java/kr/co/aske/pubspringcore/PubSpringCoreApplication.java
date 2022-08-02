4cffa3ac825d2ee95a5a80a2d6615dfcdf9d54e9
askePhoenix <aistarteverything@gmail.com>
2022-08-03 08:18:55 +0900

    Initial commit

---
+++ src/main/java/kr/co/aske/pubspringcore/PubSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package kr.co.aske.pubspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubSpringCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/aske/pubspringcore/PubSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.aske.pubspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubSpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
